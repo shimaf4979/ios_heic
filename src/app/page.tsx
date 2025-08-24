2eb1169ef951eee8c40f0e127a9818d9d45c70ad
shimayuu <cnt19024@ict.nitech.ac.jp>
2025-08-24 19:31:48 +0900

    nyaoo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,12 +33,7 @@ export default function App() {
       {/* accept 切り替え */}
       <div style={{ marginBottom: "1em" }}>
         <label>
-          <input
-            type="radio"
-            value="image/*"
-            checked={accept === "image/*"}
-            onChange={() => setAccept("image/*")}
-          />
+          <input type="radio" value="image/*" accept="image/*" />
           accept=image/*
         </label>
         <label style={{ marginLeft: "1em" }}>
